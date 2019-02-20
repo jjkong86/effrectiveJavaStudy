59db59141fde3f22f560751c03f06b5cd00b6e8a
정재공 <jjkong86@gmail.com>
2019-02-20 13:04:00 +0900

    no message

--- src/effrectiveJavaStudy/chapter2/item1/Character.java
+++ src/effrectiveJavaStudy/chapter2/item1/Character.java
@@ -2,6 +2,20 @@ package effrectiveJavaStudy.chapter2.item1;
 
 class Character {
 
+	/*
+	 * 1. 이름을 가질 수 있다.
+	 * 2. 호출될 때마다 인스턴스를 새로 생성하지 않아도 된다.
+	 * 3. 반환 타입의 하위 타입 객체를 반환할 수 있는 능력이 있다.
+	 * 4. 입력 매개변수에 따라 매번 다른 클래스의 객체를 반환할 수 있다.
+	 * 5. 정적 팩토리 메소드를 작성하는 시점에는 반환할 객체의 클래스가 존재하지 않아도 된다.
+	 * 
+	 * 단점) 
+	 * 1. 정적 팩토리만 제공하면 하위 클래스를 만들 수 없다.
+	 * 2. 정적 팩토리 매소드는 프로그래머가 찾기 어렵다 -> 문서화 필요
+	 * 
+	 * 
+	 * */
+	
     int intelligence, strength, hitPoint, magicPoint;
 
     public Character(int intelligence, int strength, int hitPoint, int magicPoint) {
--- src/effrectiveJavaStudy/chapter2/item2/Pizza.java
+++ src/effrectiveJavaStudy/chapter2/item2/Pizza.java
@@ -4,6 +4,13 @@ import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Set;
 
+	/*
+	 * 빌더 패턴
+	 *  - 필수 매개변수만으로 생성자(혹은 정적 팩토리)를 호출해 빌더 객체를 얻음
+	 *  - 그런 다음 빌더 객체가 제공하는 일종의 세터 메소드들로 원하는 선택 매개변수들을 설정
+	 *  - 매개변수가 없는 build 매소드를 호출해 필요한 객체(보통 불변)를 얻는다. 
+	 * */
+
 public abstract class Pizza {
 	public enum Topping {HAM, MUSHROOM, ONION, PERPPER, SAUSAGE}
 	final Set<Topping> toppings;
