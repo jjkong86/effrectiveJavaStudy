b5b5d1a1835478fa2595da9b747be790f9714152
정재공 <jjkong86@gmail.com>
2019-04-04 17:50:28 +0900

    no message

--- src/effrectiveJavaStudy/chapter5/Animal.java
+++ src/effrectiveJavaStudy/chapter5/Animal.java
@@ -7,7 +7,9 @@ public abstract class Animal<T> {
 	abstract T extexdClass();
 	abstract void go();
 	public static <T extends Animal<? extends T>> void doAction(List<T> animal) {
-		animal.add((T) new AnimalCat()); // 제네릭(Generics)은 항상 형 안정성을 제공하는 것이 아니다. 컴파일 시점에만 제공하고, 런타임 시점에는 형 안정성을 제공하지 못한다.
+		animal.add((T) new AnimalCat()); 
+		// 제네릭(Generics)은 항상 형 안정성을 제공하는 것이 아니다. 컴파일 시점에만 제공하고, 런타임 시점에는 형 안정성을 제공하지 못한다.
+		// 제네릭은 소거자(effective java 번역에선 소거자 라고 표현하더라 원문에선 erasure)에 의해 구현되어 컴파일시에만 자신의 타입 제약을 지키게 하고 런타임시에는 자신의 타입정보를 소거해버린다
 		for (T t : animal) {
 			t.go();
 		}
